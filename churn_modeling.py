c99bac7a02a2df4c2e183b79b2186aa3e9ed36a6
iyami60 <47905068+iyami60@users.noreply.github.com>
2020-09-08 15:23:38 +0100

    testing custumer

--- churn_modeling.py
+++ churn_modeling.py
@@ -85,3 +85,18 @@ cm = confusion_matrix(y_test, y_pred)
 print(cm)
 accuracy = (cm[0][0]+cm[1][1])/dataset.shape[0]
 print(accuracy)
+
+# Predicting a single new observation
+"""Predict if the customer with the following informations will leave the bank:
+Geography: France
+Credit Score: 600
+Gender: Male
+Age: 40
+Tenure: 3
+Balance: 60000
+Number of Products: 2
+Has Credit Card: Yes
+Is Active Member: Yes
+Estimated Salary: 50000"""
+new_prediction = classifier.predict(sc.transform(np.array([[0.0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])))
+new_prediction = (new_prediction > 0.5)
