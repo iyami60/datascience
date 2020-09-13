8e22ad764c0a72735bd64c6dd6ca5e9e6f10bf7a
iyami60 <47905068+iyami60@users.noreply.github.com>
2020-09-13 12:20:40 +0100

    update1.1

--- spyder1.py
+++ spyder1.py
@@ -30,8 +30,8 @@ train_batches = ImageDataGenerator().flow_from_directory(train_path,target_size=
 test_batches = ImageDataGenerator().flow_from_directory(train_path,target_size=(256,256),classes = ['dogs','cats'],batch_size=4)
 
 def plots(ims, figsize = (12,6),rows = 1,interp=False,titles=None):
-    if type(img[0]) is np.ndarray:
-        oms = np.array(ims).astype(np.uint8)
+    if type(ims[0]) is np.ndarray:
+        ims = np.array(ims).astype(np.uint8)
         if(ims.shape[-1]!=3):
             ims = ims.transpose((0,3,2,1))
     f = plt.figure(figsize=figsize)
@@ -40,8 +40,11 @@ def plots(ims, figsize = (12,6),rows = 1,interp=False,titles=None):
         sp =f.add_subplot(rows,cols,i+1)
         sp.axis('Off')
         if titles is not None:
-            sp.set_title(title[i],fontsize=16)
+            sp.set_title(titles[i],fontsize=16)
         plt.imshow(ims[i],interpolation = None if interp else 'none')
+
+imgs,labels  = next(train_batches)
+plots(imgs,titles=labels)
         
     
 
